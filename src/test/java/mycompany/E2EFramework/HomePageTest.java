000706500f054d0a48eb6921d609628d287661c2
prafullan82 <prafullan@gmail.com>
2019-08-26 20:18:06 +0530

    updated with new changes

--- src/test/java/mycompany/E2EFramework/HomePageTest.java
+++ src/test/java/mycompany/E2EFramework/HomePageTest.java
@@ -7,8 +7,6 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
-
 import pageObjects.LandingPage;
 import pageObjects.LoginPage;
 //@Listeners(mycompany.E2EFramework.Listners.class)
